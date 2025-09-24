16ce074aa43516beb2adb9bac55e823e1af53dee
Nidhisaa <senthilnidhi57@gmail.com>
2025-09-24 06:00:49 +0530

    basic functionality

---
+++ ExpressJS/router/route.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const User = require("../models/schema");
+
+router.get("/home", (req, res) => {
+  res.send("HELLO");
+});
+
+router.get("/home/:name", (req, res) => {
+  res.send(`HELLO ${req.params.name}`)  ;
+});
+
+module.exports = router;
--- ExpressJS/server.js
+++ ExpressJS/server.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const connectDB = require('./DB/DataBase');
+const express = require("express");
+const connectDB = require("./DB/DataBase");
 connectDB();
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
+const router = require("./router/route");
+
+app.use("/refresh", router);
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
